656908590f9d1496ed3e9d58cde1f37d145c3a68
Maksym Kostiv <rafik9202@gmail.com>
2025-04-23 15:54:04 +0300

    change styles on icons

--- src/modules/_shared/components/atoms/icons/IconWithCounter/IconWithCounter.tsx
+++ src/modules/_shared/components/atoms/icons/IconWithCounter/IconWithCounter.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import './IconWithCounter.module.scss';
+import styles from './IconWithCounter.module.scss';
 
-type Props = {
+interface Props {
   icon: string;
   count: number;
   alt?: string;
-};
+}
 
 const IconWithCounter: React.FC<Props> = ({ icon, count, alt }) => {
   return (
-    <div className="icon-wrapper">
-      <img src={icon} alt={alt} className="icon-image" />
+    <div className={styles.iconWrapper}>
+      <img src={icon} alt={alt} className={styles.iconImage} />
 
-      {count > 0 && <div className="icon-badge">{count}</div>}
+      {count > 0 && <div className={styles.iconBadge}>{count}</div>}
     </div>
   );
 };
