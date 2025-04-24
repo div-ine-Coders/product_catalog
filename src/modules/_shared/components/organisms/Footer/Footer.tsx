0482393bb544bf61bcdd08c61f1ffbb56fee3a25
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-24 15:08:44 +0300

    feat(dev-vlad)/fixed grid layout,add sticky position for footer,delete button styles from footer.module

--- src/modules/_shared/components/organisms/Footer/Footer.tsx
+++ src/modules/_shared/components/organisms/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import logo from '..//..//..//..//..//assets/icons/Logo.png';
 import * as React from 'react';
 import cn from 'classnames';
 import { ArrowButton } from '../../atoms/ArrowButton';
+import { ArrowDirection } from '../../../../../constants/ArrowDirection';
 
 export const Footer = () => {
   const handleBackClick = () => {
@@ -11,40 +12,38 @@ export const Footer = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles['footerContainer']}>
-        <div className={styles['footerLogo']}>
+      <div className={styles.footerContainer}>
+        <div className={styles.footerLogo}>
           <img src={logo} alt="Logo" />
         </div>
 
-        <div className={styles['footerNavContainer']}>
-          <ul className={styles['footerNav']}>
-            <li>
-              <a href="#" className="uppercase">
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="#" className="uppercase">
-                Contacts
-              </a>
-            </li>
-            <li>
-              <a href="#" className="uppercase">
-                Rights
-              </a>
-            </li>
-          </ul>
+        <div className={styles.footerNavContainer}>
+          <a href="#" className="uppercase" aria-label="Visit GitHub page">
+            GitHub
+          </a>
+
+          <a href="#" className="uppercase" aria-label="Contact us">
+            Contacts
+          </a>
+
+          <a
+            href="#"
+            className="uppercase"
+            aria-label="View rights information"
+          >
+            Rights
+          </a>
         </div>
 
-        <div className={styles['footerBackContainer']}>
-          <span className={cn('smallText', styles['footerBackContainerText'])}>
+        <div className={styles.footerBackContainer}>
+          <span className={cn('smallText', styles.footerBackContainerText)}>
             Back to top
           </span>
           <div
-            className={styles['footerBackContainerButton']}
+            className={styles.footerBackContainerButton}
             onClick={handleBackClick}
           >
-            <ArrowButton />
+            <ArrowButton direction={ArrowDirection.Up} isDisabled={false} />
           </div>
         </div>
       </div>
