736af22b983ffbf0a0678751418a72fec950111d
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-25 23:45:00 +0300

    style(DEV)/some changes in styles

--- src/modules/_shared/components/molecules/ProductCard/ProductCard.tsx
+++ src/modules/_shared/components/molecules/ProductCard/ProductCard.tsx
@@ -16,101 +16,100 @@ export const ProductCard: React.FC = () => {
   const [isFavourite, setIsFavourite] = useState(false);
 
   return (
-    <div className={styles.productCardContainer}>
-      <div className={styles.productCard}>
-        <a href="#">
-          <img
-            src={photo}
-            alt="Apple iPhone Xs 64GB Silver (iMT9G2FS/A)"
-            className={styles.productCardImage}
-          />
-        </a>
-
-        <p className={styles.productCardTitle}>
-          Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
-        </p>
-        {/*here will be NavLink */}
-
-        <div className={styles.productCardPrices}>
-          <h3 className={styles.productCardPricesCurrent}>$799</h3>
-          <h3 className={styles.productCardPricesFull}>$899</h3>
-        </div>
-
-        <hr className={styles.productCardBorder} />
-
-        <ul className={styles.productCardDetails}>
-          <li className={cn('smallText', styles.productCardDetailsText)}>
-            Screen: <span>5.8” OLED</span>
-          </li>
-          <li className={cn('smallText', styles.productCardDetailsText)}>
-            Capacity: <span>64 GB</span>
-          </li>
-          <li className={cn('smallText', styles.productCardDetailsText)}>
-            RAM: <span>4 GB</span>
-          </li>
-        </ul>
-
-        <div className={styles.productCardButtons}>
-          <div className={styles.productCardButtonsDefault}>
-            <DefaultButton isSelected={inCart} click={() => setInCart(!inCart)}>
-              {inCart ? 'Added' : 'Add to cart'}
-            </DefaultButton>
-          </div>
-
-          <div className={styles.productCardButtonsFavourite}>
-            <FavouriteButton
-              isFavourite={isFavourite}
-              click={() => setIsFavourite(!isFavourite)}
-            />
-          </div>
-        </div>
-      </div>
-
-      {/* use this div after aprove Pull Request*/}
-      {/* <div className={styles.productCard}>
+    <div className={styles.productCard}>
+      <a href="#" className={styles.productCardLink}>
         <img
-           src={`/${product.image}`}
-          alt={product.name}
+          src={photo}
+          alt="Apple iPhone Xs 64GB Silver (iMT9G2FS/A)"
           className={styles.productCardImage}
         />
-        <p className={styles.productCardTitle}>
-        {product.name}
-        </p>
+      </a>
 
-        <div className={styles.productCardPrices}>
-          <span className={styles.productCardPricesCurrent}>${product.price}</span>
-          <span className={styles.productCardPricesFull}>${product.fullPrice}</span>
+      <p className={styles.productCardTitle}>
+        Apple iPhone Xs 64GB Silver (iMT9G2FS/A)
+      </p>
+      {/*here will be NavLink */}
+
+      <div className={styles.productCardPrices}>
+        <h3 className={styles.productCardPricesCurrent}>$799</h3>
+        <h3 className={styles.productCardPricesFull}>$899</h3>
+      </div>
+
+      <div className={styles.productCardBorder} />
+
+      <ul className={styles.productCardDetails}>
+        <li className={cn('smallText', styles.productCardDetailsText)}>
+          Screen: <span>5.8” OLED</span>
+        </li>
+        <li className={cn('smallText', styles.productCardDetailsText)}>
+          Capacity: <span>64 GB</span>
+        </li>
+        <li className={cn('smallText', styles.productCardDetailsText)}>
+          RAM: <span>4 GB</span>
+        </li>
+      </ul>
+      <div className={styles.productCardButtons}>
+        <div className={styles.productCardButtonsDefault}>
+          <DefaultButton isSelected={inCart} click={() => setInCart(!inCart)}>
+            {inCart ? 'Added' : 'Add to cart'}
+          </DefaultButton>
         </div>
 
-        <div className={styles.productCardBorder}></div>
-
-        <div className={styles.productCardDetails}>
-          <p className={styles.productCardDetailsText}>
-            Screen: <span>{product.screen}</span>
-          </p>
-          <p className={styles.productCardDetailsText}>
-            Capacity: <span>{product.capacity}</span>
-          </p>
-          <p className={styles.productCardDetailsText}>
-            RAM: <span>{product.ram}</span>
-          </p>
+        <div className={styles.productCardButtonsFavourite}>
+          <FavouriteButton
+            isFavourite={isFavourite}
+            click={() => setIsFavourite(!isFavourite)}
+          />
         </div>
-
-        <div className={styles.productCardButtons}>
-          <div className={styles.productCardButtonsDefault}>
-            <DefaultButton isSelected={inCart} click={() => setInCart(!inCart)}>
-              {inCart ? 'Added' : 'Add to cart'}
-            </DefaultButton>
-          </div>
-
-          <div className={styles.productCardButtonsFavourite}>
-            <FavouriteButton
-              isFavourite={isFavourite}
-              click={() => setIsFavourite(!isFavourite)}
-            />
-          </div>
-        </div>
-      </div> */}
+      </div>
     </div>
+    //<div className={styles.productCardContainer}>
+
+    // {/* use this div after aprove Pull Request*/}
+    // {/* <div className={styles.productCard}>
+    //   <img
+    //      src={`/${product.image}`}
+    //     alt={product.name}
+    //     className={styles.productCardImage}
+    //   />
+    //   <p className={styles.productCardTitle}>
+    //   {product.name}
+    //   </p>
+
+    //   <div className={styles.productCardPrices}>
+    //     <span className={styles.productCardPricesCurrent}>${product.price}</span>
+    //     <span className={styles.productCardPricesFull}>${product.fullPrice}</span>
+    //   </div>
+
+    //   <div className={styles.productCardBorder}></div>
+
+    //   <div className={styles.productCardDetails}>
+    //     <p className={styles.productCardDetailsText}>
+    //       Screen: <span>{product.screen}</span>
+    //     </p>
+    //     <p className={styles.productCardDetailsText}>
+    //       Capacity: <span>{product.capacity}</span>
+    //     </p>
+    //     <p className={styles.productCardDetailsText}>
+    //       RAM: <span>{product.ram}</span>
+    //     </p>
+    //   </div>
+
+    //   <div className={styles.productCardButtons}>
+    //     <div className={styles.productCardButtonsDefault}>
+    //       <DefaultButton isSelected={inCart} click={() => setInCart(!inCart)}>
+    //         {inCart ? 'Added' : 'Add to cart'}
+    //       </DefaultButton>
+    //     </div>
+
+    //     <div className={styles.productCardButtonsFavourite}>
+    //       <FavouriteButton
+    //         isFavourite={isFavourite}
+    //         click={() => setIsFavourite(!isFavourite)}
+    //       />
+    //     </div>
+    //   </div>
+    // </div> */}
+    // </div>
   );
 };
