a80b720892c98a69f55c3a298a15c593d085bae5
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-25 11:00:45 +0300

    add <ProductCard/> to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import './App.scss';
 import { Header } from 'modules/_shared/components/organisms/Header';
 import { Footer } from 'modules/_shared/components/organisms/Footer';
+// eslint-disable-next-line max-len
+import { ProductCard } from 'modules/_shared/components/molecules/ProductCard/ProductCard';
 
 export const App = () => {
   return (
@@ -10,6 +12,7 @@ export const App = () => {
 
       <div className="main">
         {/* тут можна тестити поки немає -> <Outlet /> */}
+        <ProductCard />
       </div>
 
       <div className="footer">
