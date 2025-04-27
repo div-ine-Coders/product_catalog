7e822a468a2300a4caa82e9ffc57a0a82f031b32
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-27 22:34:06 +0300

    changes in CatalogList.tsx and add some styles to ProductCard

--- src/modules/_shared/components/molecules/CatalogList/CatalogList.tsx
+++ src/modules/_shared/components/molecules/CatalogList/CatalogList.tsx
@@ -4,18 +4,16 @@ import styles from './CatalogList.module.scss';
 import React from 'react';
 
 export const CatalogList = () => {
-  
-
   return (
     <div className={styles.CatalogListContainer}>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
-      <ProductCard product={undefined}/>
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
+      <ProductCard product={undefined} />
     </div>
   );
 };
