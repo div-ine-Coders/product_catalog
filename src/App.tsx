5615d69b8f6523f0009e862cb3d6b46aa3a4a7d0
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-27 22:22:13 +0300

    feat/catalog-list

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,53 @@ import * as React from 'react';
 import './App.scss';
 import { Header } from 'modules/_shared/components/organisms/Header';
 import { Footer } from 'modules/_shared/components/organisms/Footer';
+// import { CartItem } from './modules/_shared/components/molecules/CartItem';
+import { CatalogList } from 'modules/_shared/components/molecules/CatalogList';
+
+// const item1 = {
+//   id: 1,
+//   category: 'phones',
+//   itemId: 'apple-iphone-7-32gb-black',
+//   name: 'Apple iPhone 7 32GB Black',
+//   fullPrice: 400,
+//   price: 375,
+//   screen: "4.7' IPS",
+//   capacity: '32GB',
+//   color: 'black',
+//   ram: '2GB',
+//   year: 2016,
+//   image: 'img/phones/apple-iphone-7/black/00.webp',
+// };
+
+// const item2 = {
+//   id: 2,
+//   category: 'phones',
+//   itemId: 'apple-iphone-7-plus-32gb-black',
+//   name: 'Apple iPhone 7 Plus 32GB Black',
+//   fullPrice: 540,
+//   price: 500,
+//   screen: "5.5' IPS",
+//   capacity: '32GB',
+//   color: 'black',
+//   ram: '3GB',
+//   year: 2016,
+//   image: 'img/phones/apple-iphone-7-plus/black/00.webp',
+// };
+
+// const item3 = {
+//   id: 3,
+//   category: 'phones',
+//   itemId: 'apple-iphone-8-64gb-gold',
+//   name: 'Apple iPhone 8 64GB Gold',
+//   fullPrice: 600,
+//   price: 550,
+//   screen: "4.7' IPS",
+//   capacity: '64GB',
+//   color: 'gold',
+//   ram: '2GB',
+//   year: 2017,
+//   image: 'img/phones/apple-iphone-8/gold/00.webp',
+// };
 import { Outlet } from 'react-router-dom';
 
 export const App = () => {
@@ -9,6 +56,18 @@ export const App = () => {
     <div className="App">
       <Header />
 
+      <div className="main">
+        {/* <div style={{ margin: '20px' }}>
+          <CartItem good={item1} />
+        </div>
+        <div style={{ margin: '20px' }}>
+          <CartItem good={item2} />
+        </div>
+        <div style={{ margin: '20px' }}>
+          <CartItem good={item3} />
+        </div> */}
+        <CatalogList />
+      </div>
       <main>{<Outlet />}</main>
 
       <div className="footer">
