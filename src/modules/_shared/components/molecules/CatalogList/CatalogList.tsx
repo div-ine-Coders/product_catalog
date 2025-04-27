fd5ccacb73acc29b073a8496791b6af6046389f9
Vladyslav Rozmarytsia <vladroz1995@gmail.com>
2025-04-27 22:35:32 +0300

    add commet for CatalogList

--- src/modules/_shared/components/molecules/CatalogList/CatalogList.tsx
+++ src/modules/_shared/components/molecules/CatalogList/CatalogList.tsx
@@ -7,6 +7,7 @@ export const CatalogList = () => {
   return (
     <div className={styles.CatalogListContainer}>
       <ProductCard product={undefined} />
+      {/* just for testing remain like this product={undefined}*/}
       <ProductCard product={undefined} />
       <ProductCard product={undefined} />
       <ProductCard product={undefined} />
