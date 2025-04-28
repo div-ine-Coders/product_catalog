e98a65b0d9c8167b618c6d1ded516a70fb53868c
Roman Ohirok <matsuhita.2307@gmail.com>
2025-04-28 11:04:50 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.scss';
 import { Header } from 'modules/_shared/components/organisms/Header';
 import { Footer } from 'modules/_shared/components/organisms/Footer';
 import { Outlet } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
 
 export const App = () => {
   return (
