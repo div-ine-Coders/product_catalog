63de3b1a9f8bf60c22554195488bcd2f2c82c98d
Vitalii Hutsalo <jacklounter07@gmail.com>
2025-04-28 23:49:20 +0300

    final fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.scss';
 import { Header } from 'modules/_shared/components/organisms/Header';
 import { Footer } from 'modules/_shared/components/organisms/Footer';
 import { Outlet } from 'react-router-dom';
+// eslint-disable-next-line max-len
+import { ShopByCategory } from 'modules/HomePage/components/ShopByCategory/ShopByCategory';
 
 export const App = () => {
   return (
