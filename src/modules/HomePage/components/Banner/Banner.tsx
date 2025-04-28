7a33f49562291845b3d15f685f240c62f8d2e21e
Vitalii Hutsalo <jacklounter07@gmail.com>
2025-04-28 12:16:40 +0300

    DEV-Vitalii_implement shop by category

---
+++ src/modules/HomePage/components/Banner/Banner.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Banner.module.scss';
+import banner from '@assets/icons/category-accessories.png';
+
+export const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.bannerContent}>
+        <h2 className={styles.bannerTitle}>
+          Now available
+          <br />
+          in our store!👌
+        </h2>
+
+        {/* Цей блок буде тільки на планшеті і десктопі */}
+        <div className={styles.bannerDesktopContent}>
+          <p className={styles.bannerText}>Be the first</p>
+          <button className={styles.bannerButton}>Order Now</button>
+        </div>
+      </div>
+
+      <div className={styles.bannerImageWrapper}>
+        <img className={styles.bannerImage} src={banner} alt="iPhone 14 Pro" />
+      </div>
+    </div>
+  );
+};
