c05682fd34a0dcdb0c022f88d3b89ce2a24ca1a8
Vitalii Hutsalo <jacklounter07@gmail.com>
2025-04-30 17:19:32 +0300

    vitalii work version of main slider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import './App.scss';
 import { Header } from 'modules/_shared/components/organisms/Header';
 import { Footer } from 'modules/_shared/components/organisms/Footer';
 import { Outlet } from 'react-router-dom';
-// eslint-disable-next-line max-len
-import { useSyncSearchParamsWithStore } from '@hooks/effectHooks/useSearchParamsSync';
-import { Breadcrumbs } from 'modules/_shared/components/molecules/Breadcrumbs';
-import { HomePage } from 'modules/HomePage';
-
-import { HomePage } from 'modules/HomePage/HomePage';
 
 export const App = () => {
   useSyncSearchParamsWithStore();
@@ -16,12 +10,8 @@ export const App = () => {
   return (
     <div className="App">
       <Header />
-      <Breadcrumbs />
-      <HomePage />
-      <main>
-        {/* тут більше нічого не пишемо переходьте на сторінку, яка вам потрібна*/}
-        <Outlet />
-      </main>
+
+      <main>{<Outlet />}</main>
       <div className="footer">
         <Footer />
       </div>
