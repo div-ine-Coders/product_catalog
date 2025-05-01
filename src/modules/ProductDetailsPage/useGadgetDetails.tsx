825fc73981037f01acbe92e3d524678c09ae686d
Roman Ohirok <matsuhita.2307@gmail.com>
2025-05-01 16:48:39 +0300

    disable array of dependencies for a useGadgetsDetail

--- src/modules/ProductDetailsPage/useGadgetDetails.tsx
+++ src/modules/ProductDetailsPage/useGadgetDetails.tsx
@@ -93,6 +93,7 @@ export const useGadgetDetails = () => {
     };
 
     loadDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [productId]);
 
   const displayGadget = gadget || previousGadget.current;
